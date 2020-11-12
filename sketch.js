7502a7d6799d8303a6c32118fcd1127d867d7710
Aedan8142008 <annycole73@gmail.com>
2020-11-12 17:45:02 -0600

    added

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
 var ball;
+var database,dballpos;
+var position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    dballpos = database.ref('ball/position');
+    dballpos.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function readPosition(data)
+{
+    position = data.val();//x& y pos from db
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError()
+{
+    console.log("Error");
+}
+
+
+function writePosition(x,y){
+database.ref('ball/position').set({
+    x:position.x + x,
+    y:position.y + y
+})
+
 }
